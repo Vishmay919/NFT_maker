e6bf21afc751edd504c65916cf040e2b2332550a
Vishmay <vishmaychauhan11@gmail..com>
2022-01-08 10:45:19 +0530

    initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
+  //0x54e55372FadD9b3a0051A47cb7a40E9c5551dCB9
